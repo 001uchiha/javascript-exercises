9f70fc7a8b1ca4c2b2be42ef930754692e578e26
001uchiha <mohaking321@gmail.com>
2025-06-21 18:02:10 +0200

    Compelete 06_repeatString exercise solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+	if (n < 0) return "ERROR";
+	
+	strCount = ''
+  	for (let i = 0; i < n; i++) {
+    	strCount += str;
+  	}
 
+  	return strCount;
 };
 
 // Do not edit below this line
