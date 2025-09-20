9576d7504768e09f44b5f07fcfa574017e434017
001uchiha <mohaking321@gmail.com>
2025-09-20 20:49:02 +0200

    Complete 13_palindromes exercise solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+	const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+	return cleaned === [...cleaned].reverse().join('');
 };
 
 // Do not edit below this line
